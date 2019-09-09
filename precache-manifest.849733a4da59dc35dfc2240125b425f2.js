7c11082cc7e07381f109eba95ef5b89e759ddbe9
muhsinwarfa <muhsin.warfa@gmail.com>
2019-09-09 21:44:13 +0300

    Updates

---
+++ precache-manifest.849733a4da59dc35dfc2240125b425f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e5b27813f71d19b47beb644fbd2a878",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "7c250a140dc6793bbcd3",
+    "url": "/react-todo-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "6bee0f44307c0401e918",
+    "url": "/react-todo-app/static/css/main.c53f3d1c.chunk.css"
+  },
+  {
+    "revision": "7c250a140dc6793bbcd3",
+    "url": "/react-todo-app/static/js/2.9a9d4a25.chunk.js"
+  },
+  {
+    "revision": "6bee0f44307c0401e918",
+    "url": "/react-todo-app/static/js/main.dcd0d509.chunk.js"
+  },
+  {
+    "revision": "789c660835dccb272226",
+    "url": "/react-todo-app/static/js/runtime~main.6a7bf040.js"
+  }
+]);
